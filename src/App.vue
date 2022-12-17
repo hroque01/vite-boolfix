794dc15e993e8975899ae59d1fed15ca2b5ec6d3
Hadson Roque <hadson.roque01@git config --global user.email hadson.roque01@gmail.com>
2022-12-18 02:10:22 +0800

    Create Appheader.vue template and added to App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,26 @@
 <script >
+import AppHeaderVue from './components/AppHeader.vue';
 
+// import store js
+// import { store } from './store.js';
+
+export default {
+  name: "App",
+  components: {
+    AppHeaderVue,
+  },
+  // data() {
+  //   return {
+  //     store,
+  //   }
+  // }
+}
 </script>
 
 <template>
-
+  <AppHeaderVue />
 </template>
 
-<style scoped>
-
+<style lang="scss" scoped>
+@use "./styles/general.scss" as *;
 </style>
