68b8182368af5c26a8bba29f9bc86ca056dd03b1
Hadson Roque <hadson.roque01@git config --global user.email hadson.roque01@gmail.com>
2022-12-18 18:59:59 +0800

    Add SingleFilm.vue

---
+++ src/components/SingleFilm.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+    name: 'SingleFilm',
+    props: [],
+}
+</script>
+
+<template>
+    <div class="card">
+        <!-- template di card singola -->
+        <img src="" alt="">
+        <h4>Titolo</h4>
+        <div>Info</div>
+        <div>Info2</div>
+    </div>
+</template>
+
+<style lang="scss">
+@use "../styles/partials/variables.scss" as *;
+</style>
