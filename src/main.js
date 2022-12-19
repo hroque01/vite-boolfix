940ef701b39faee23b89690d702d8d13abf3b397
Hadson Roque <hadson.roque01@git config --global user.email hadson.roque01@gmail.com>
2022-12-20 00:53:57 +0800

    Add Fontawesome in main.js

--- src/main.js
+++ src/main.js
@@ -1,4 +1,12 @@
 import { createApp } from 'vue'
+
+// Fontawesome
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faHatWizard } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
 import App from './App.vue'
 
-createApp(App).mount('#app')
+library.add(faHatWizard)
+
+createApp(App).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
