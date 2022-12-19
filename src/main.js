cc64a87366dcb372dd19e43b4f677d44f0df623c
Hadson Roque <hadson.roque01@git config --global user.email hadson.roque01@gmail.com>
2022-12-20 01:47:14 +0800

    Remove fontawesome

--- src/main.js
+++ src/main.js
@@ -1,14 +1,5 @@
 import { createApp } from 'vue'
 
-import { library } from '@fortawesome/fontawesome-svg-core'
-
-import { faHatWizard } from '@fortawesome/free-solid-svg-icons'
-
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-
 import App from './App.vue'
 
-library.add(faHatWizard)
-
-
-createApp(App).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
+createApp(App).mount('#app')
