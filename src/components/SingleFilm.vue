c02a8e5240c891166671141200afe81a952dac70
Hadson Roque <hadson.roque01@git config --global user.email hadson.roque01@gmail.com>
2022-12-20 00:32:39 +0800

    Add Flags language in SingeFilm.vue

--- src/components/SingleFilm.vue
+++ src/components/SingleFilm.vue
@@ -2,7 +2,48 @@
 export default {
     name: 'SingleFilm',
     props: ['info'],
-}
+    data() {
+        return {
+            flags: [
+                {
+                    img: 'img/france.png',
+                    lang: "fr"
+                },
+                {
+                    img: 'img/german-flag.png',
+                    lang: "de"
+                },
+                {
+                    img: 'img/italy.png',
+                    lang: "it"
+                },
+                {
+                    img: 'img/united-kingdom.png',
+                    lang: "en"
+                },
+                {
+                    img: 'img/spain.png',
+                    lang: "es"
+                },
+                {
+                    img: 'img/rainbow.png',
+                    lang: ""
+                },
+            ]
+        }
+    },
+    computed: {
+        flagEmoji() {
+
+            for (let i = 0; i < this.flags.length; i++) {
+                if (this.flags[i].lang.includes(this.info.original_language)) {
+                    return this.flags[i].img
+                }
+            }
+            return this.flags[this.flags.length - 1].img
+        }
+    }
+}   
 </script>
 
 <template>
@@ -11,10 +52,19 @@ export default {
         <img :src="`https://image.tmdb.org/t/p/w200/${info.poster_path}`" alt="">
         <div>Titolo: {{ info.title }}</div>
         <div>Titolo Originale: {{ info.original_title }}</div>
-        <div>Info2</div>
+        <div class="flags">
+            <span> Language:</span>
+            <img :src="flagEmoji" alt="">
+        </div>
     </div>
 </template>
 
 <style lang="scss">
 @use "../styles/partials/variables.scss" as *;
+
+.flags {
+    img {
+        width: 50px;
+    }
+}
 </style>
